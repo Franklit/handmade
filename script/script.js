1432ab62e84bafe106f8d002dcb612a08d8c8851
Jakub <jakub.jadczyk@wp.pl>
2020-09-24 17:43:11 +0200

    add lightbox, css style,

--- script/script.js
+++ script/script.js
@@ -33,5 +33,16 @@ function scrollFunction() {
     // if(document.documentElement.scrollTop > 80){
     //     nav.style.top = '-100px'
     // }
-    document.documentElement.scrollTop > 80 ? nav.style.top = '-150px' : nav.style.top = '0px'
+    document.documentElement.scrollTop > 80 ? nav.style.top = '-100px' : nav.style.top = '0px'
 }
+
+
+// checkProduct
+const checkProduct = document.querySelector(' .top h1 a'); 
+
+checkProduct.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: document.querySelector(`#kolczyki`).offsetTop,
+        behavior: 'smooth'
+    })
+});
